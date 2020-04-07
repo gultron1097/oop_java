700d906191c8f9d4a1d3b3ad8cfa52794bdb3218
gultron <gultron10970@gmail.com>
2020-04-07 10:51:22 +0800

    Update Classes with LinkedList, also add FiveCardStudGame class

--- poker_demo/src/poker_demo/DeckOfCards.java
+++ poker_demo/src/poker_demo/DeckOfCards.java
@@ -1,23 +1,53 @@
 package poker_demo;
 
+import java.util.*;
+
 public class DeckOfCards {
+	private LinkedList<Card> cardList;
 	
 	static int NUM_OF_CARDS = 52;
-	Card[] cards = new Card[NUM_OF_CARDS]; //it stores 52 cards
+	
 	
 	public DeckOfCards()
 	{
+		cardList = new LinkedList<Card>();
 		for(int i = 1; i <= NUM_OF_CARDS; i++)
 		{
-			cards[i-1] = new Card(i);
+			
+			this.put(new Card(i));
 		}
 	}
 	
+	public DeckOfCards(boolean is_shuffle) {
+		cardList = new LinkedList<Card>();
+		
+		for(int i = 0; i < DeckOfCards.NUM_OF_CARDS; i++)
+		{
+			this.put(new Card(i+1));
+		}
+		if(is_shuffle)
+			this.shuffle();
+	}
+	
+	public void put(Card card)
+	{
+		cardList.addFirst(card);
+	}
+	
+	public Card get() {
+		return cardList.removeLast();
+	}
+	
+	
+	public int cardCount()
+	{
+		return cardList.size();
+	}
 	public void displayCards()
 	{
-		for (int i=0; i<DeckOfCards.NUM_OF_CARDS; i++)
+		for (int i=0; i<cardList.size(); i++)
 		{
-			Card card = cards[i];
+			Card card = cardList.get(i);
 			System.out.println(card.getPattern() + card.getNumber());
 		}
 	}
@@ -33,9 +63,9 @@ public class DeckOfCards {
 			if(i!=j)
 			{
 				//Swap to cards
-				Card temp = cards[i];
-				cards[i] = cards[j];
-				cards[j] = temp;
+				Card temp = cardList.get(i);
+				cardList.set(i, cardList.get(j));
+				cardList.set(j,temp); 
 			}
 			else {
 				continue;
@@ -46,13 +76,13 @@ public class DeckOfCards {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DeckOfCards myDeck = new DeckOfCards();
-		System.out.println("Before shuffling, the deck of cards is as follos:");
+		System.out.println("Before shuffling, the deck of cards is as follows:");
 		myDeck.displayCards();
 		
 		//Invoke/call the method Shuffle
 		myDeck.shuffle();
 		
-		System.out.println("After shuffling, the deck of cards is as follos:");
+		System.out.println("After shuffling, the deck of cards is as follows:");
 		myDeck.displayCards();
 
 	}
--- poker_demo/src/poker_demo/Player.java
+++ poker_demo/src/poker_demo/Player.java
@@ -1,11 +1,48 @@
 package poker_demo;
 
-public class Player {
-	final static int MAX_CARD_COUNT = 5;
-	Card[] cards = new Card[MAX_CARD_COUNT];
+import java.util.LinkedList;
 
+public class Player {
+	
+	final static int MAX_CARDS_COUNT = 5;
+private LinkedList<Card> cardList;
+	
+	//static int NUM_OF_CARDS = 52;
+	
+	public Player()
+	{
+		cardList = new LinkedList<Card>();
+	}
+	
+	public void put(Card card)
+	{
+		cardList.addFirst(card);
+	}
+	
+	public Card get() {
+		return cardList.removeLast();
+	}
+	
+	public boolean isEmpty()
+	{
+		return cardList.size() == 0;
+	}
+	
+	public void displayCards()
+	{
+		for (int i=0; i<cardList.size(); i++)
+		{
+			Card card = cardList.get(i);
+			System.out.println(card.getPattern() + card.getNumber());
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Player player = new Player();
+		player.put(new Card(0,1)); //Club Ace
+		System.out.println("The player's cards are listed as follows:");
+		player.displayCards();
 
 	}
 
